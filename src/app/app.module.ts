b7cf0a71515476cb9c0f67eca5b4052fb0c14421
Tania <evdunova.tania@gmail.com>
2023-07-27 21:19:09 +0200

    new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    Test2Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/test1/test1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-test1',
+  templateUrl: './test1.component.html',
+  styleUrls: ['./test1.component.css']
+})
+
+export class TestComponent {
+  title = 'Test Title'
+}
